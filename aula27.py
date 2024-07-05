3cdcd49e5f68fede6a7f22823393d0d125a396a4
Everton Guatura Fernandes <everton.gfernandes@ter.grupomarista.org.br>
2024-07-05 14:33:28 -0300

    commit 3

--- aula27.py
+++ aula27.py
@@ -8,4 +8,4 @@ de caracteres da str
 '''
 
 variavel = 'ola mundo'
-print(variavel[0:9:2])
+print(variavel[-9:0])
--- aula28.py
+++ aula28.py
@@ -1,3 +1,19 @@
-'''Exercicio'''
+'''
+Exercicio
+peça ao usuario para digitar seu nome
+peça ao usuario para digitar sua idade
+se nome e idade forem digitados:
+exiba:
+    seu nome é {nome}
+    seu nome invertido é {nome invertido}
+    se nome contem  (ou não) espaços
+    seu nome tem {n} letras
+    A primeira letra do seu nome é {letra}
+se nada for digitado em nome ou idade:
+   exiba "Desculpe, voce deixou campos vazios."
+   '''
+nome = input('qual o seu nome: ')
+#idade = int(input('qual a sua idade: '))
+print(f'seu nome é :{nome}!')
+print(f'seu nome invertido fica: {nome[:-7]}')
 
-print('hello World')
