37f2845c1a45cedd5eabdf1a23e9d4ca7c81800b
Everton Guatura Fernandes <everton.gfernandes@ter.grupomarista.org.br>
2024-07-08 10:06:12 -0300

    estudando python

--- aula29.py
+++ aula29.py
@@ -4,3 +4,12 @@ try -> tentar executar o codigo
 except -> ocoprreu algum erro ao tentar executar
 """
 
+numero_str = input('vou dobrar o numero que vc digitar: ')
+
+try:
+    print('STR:' , numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT:' , numero_float)
+    print(f'o dobro do numero: {numero_str} é {numero_float * 2:.0f}')
+except:
+    print('Isso não é um numero')
