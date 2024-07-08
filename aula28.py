a1c2dab8e2a161e6344307d63bde3528697e2f2c
Everton Guatura Fernandes <everton.gfernandes@ter.grupomarista.org.br>
2024-07-08 09:37:56 -0300

    estudos atualizados

--- aula28.py
+++ aula28.py
@@ -11,9 +11,22 @@ exiba:
     A primeira letra do seu nome é {letra}
 se nada for digitado em nome ou idade:
    exiba "Desculpe, voce deixou campos vazios."
-   '''
+ '''
+
 nome = input('qual o seu nome: ')
-#idade = int(input('qual a sua idade: '))
-print(f'seu nome é :{nome}!')
-print(f'seu nome invertido fica: {nome[:-7]}')
+idade = int(input('qual a sua idade: '))
+if nome and idade:
+    print(f'seu nome é :{nome}!')
+    print(f'seu nome invertido é: {nome[::-1]}')
+    if ' ' in nome:
+        print('seu nome contem espaços')
+    else:
+        print('seu nome NÃO contem espaços.')
+    
+    print(f'a quantidade de letras para o meu nome são: {len(nome)} letras!')
+    print(f'a primeira letra do meu nome é: {nome[0]}')
+    print(f'a ultima letra do meu nome é: {nome[-1]}')
+else:
+    print("desculpe, voce deixou oos campos vazios.!")
+
 
---
+++ aula29.py
@@ -0,0 +1,6 @@
+"""
+Introdução ao try/except
+try -> tentar executar o codigo
+except -> ocoprreu algum erro ao tentar executar
+"""
+
