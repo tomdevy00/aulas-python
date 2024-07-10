7233c69966d65bce554fe0777b8b51843a5e285e
Everton Guatura Fernandes <everton.gfernandes@ter.grupomarista.org.br>
2024-07-10 14:18:10 -0300

    aula 23 de python

---
+++ aula32.py
@@ -0,0 +1,29 @@
+"""
+faça um programa que peça ao usuario para digitar um numero inteiro,
+informe se este numero é par ou impar. caso usuario não digite um numero
+inteiro, informe que não é um numero inteiro
+
+"""
+"""
+faça um programa que pergunte a hora ao usuario e, baseando-se no horario 
+descrito, exiba a saudação apropriada.Ex
+bom dia 0-11, boa tarde 12-17 e boa noite 18-23
+
+"""
+"""
+Faça um programa que peça o primeiro nome ao usuario. se o nome tiver 4 letras ou
+menos escreva "Seu nome é curto"; se tiver entre 5 letras e 6 letras, ecreva
+"Seu nome é normal"; maior que 6 ecreva "Seu nome é muito grande"
+"""
+num = int(input("digite um numero:  "))
+if num % 4 == 0:
+    ("este numero é par")
+else:
+    ("este numero é impar")
+
+
+
+
+
+
+
